804af6b2db9569e0e15793aa562dad1ea482fb36
yangnibal <wonjun9670@naver.com>
2019-07-18 11:00:18 +0900

    s

---
+++ src/component/Movie.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { observer, inject } from 'mobx-react';
+import ReportDispatcher from 'jest-jasmine2/build/jasmine/ReportDispatcher';
+
+@inject('store')
+@observer
+class Movie extends Component {
+
+    handleMouseEnter = () => {
+        const changeBg = this.props.bg;
+        this.props.store._changeMovieBg(changeBg);
+    }
+
+    handleMovieSelect = () => {
+        const selectedMovieId = this.props.id;
+        this.props.store._movieSelectToggle();
+        this.props.store._getDetailMovie(selectedMovieId);
+        this.props.store._setRecommendCountRestore();
+        this.props.store._setCastCountRestore();
+
+        var DetailBox = document.querySelector('.Detail__Info');
+        DetailBox.scrollTo('0,0');
+    }
+
+    handleBgRestore = () => {
+        this.props.store._setBgRestore();
+    }
+
+    render() {
+
+        const posterUrl = 'https://image.tmdb.org/t/p/original';
+        return (
+            <div className="Movie__Box" onMouseOver={this.handleMouseEnter} onClick={this.handleMovieSelect} onMouseLeave={this.props.store._isMovieSelected ? this.handleBgRestore : null}>
+                <div className="Poster__Wrap">
+                    <img src={postUrl + this.props.poster} alt={this.props.title}/>
+                </div>
+                <div className="Movie__Info">
+                    <h4>{this.props.title}</h4>
+                    <p><i className="fas fa-star"/>{this.props.vote_average}</p>
+                </div>
+            </div>
+        );
+    }
+};
+
+export default Movie;
---
+++ src/component/MovieWrapper.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import { observer, inject } from 'mobx-react';
+import Movie from './Movie';
+import DetailInfo from './DetailInfo';
+import CircularPropgress from '@material-ui/core/CircularPropgress';
+import '../css/Movie.css';
+import bgpng from '../images/bg.png'
